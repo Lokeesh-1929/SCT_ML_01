bb1cf534fd76cb954e801d138cc24410785252c7
Lokeesh-1929 <lokeshchowdarinidamanuri@gmail.com>
2026-05-23 08:01:47 +0530

    House Price Prediction Project

---
+++ README.md
@@ -0,0 +1,47 @@
+# House Price Prediction Using Linear Regression
+
+## Project Overview
+
+This is a beginner Machine Learning project that predicts house prices using Linear Regression.
+
+The project uses:
+
+- Python
+- Pandas
+- NumPy
+- Matplotlib
+- Scikit-learn
+
+---
+
+## Features
+
+- Load housing dataset
+- Train Linear Regression model
+- Predict house prices
+- Compare actual vs predicted prices
+- Display graph visualization
+
+---
+
+## Technologies Used
+
+| Technology | Purpose |
+|------------|---------|
+| Python | Programming Language |
+| Pandas | Data Handling |
+| NumPy | Numerical Operations |
+| Matplotlib | Data Visualization |
+| Scikit-learn | Machine Learning |
+
+---
+
+## Project Structure
+
+```text
+home_price_prediction/
+│
+├── house_price_predictin.py
+├── Housing.csv
+├── README.md
+├── venv/
---
+++ house_price_predictin.py
@@ -0,0 +1,48 @@
+print("Hello Machine Learning")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+data = pd.read_csv(r"C:\Users\teja\Desktop\home_price_prediction\Housing.csv")
+print(data.head())
+
+X = data[['area', 'bedrooms', 'bathrooms']]
+y = data['price']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("MAE:", mae)
+print("R2 Score:", r2)
+
+sample_house = [[5000, 3, 2]]
+
+prediction = model.predict(sample_house)
+
+print("Predicted Price:", prediction[0])
+
+plt.scatter(y_test, y_pred)
+
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+
+plt.title("Actual vs Predicted Prices")
+
+plt.show()
